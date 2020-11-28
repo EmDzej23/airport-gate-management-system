100fa042cd05231b30dfe8c5eda00810ac7c96ae
Marko Jereminov <markojereminov@hotmail.com>
2020-11-28 17:22:10 +0100

    Setup Spring boot application, add needed dependencies to pom

---
+++ src/main/java/com/mj/airport/AirportGateApplication.java
@@ -0,0 +1,22 @@
+package com.mj.airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author marko
+ */
+@SpringBootApplication
+public class AirportGateApplication {
+    //main
+    public static void main(String[] args) {
+        SpringApplication.run(AirportGateApplication.class, args);
+    }
+}
